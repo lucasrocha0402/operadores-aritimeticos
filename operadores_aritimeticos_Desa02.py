a531b96cf53ec8a0fa6f1401a026306fbcfa0888
Lucas Rocha <lukasrocha0402@gmail.com>
2024-03-01 09:34:32 -0300

    Adicionando o segundo desafio

---
+++ operadores_aritimeticos_Desa02.py
@@ -0,0 +1,6 @@
+valor = int(input('Digite o valor que deseja: '))
+
+dobro = valor * 2
+triplo = valor * 3
+raiz = valor ** (1/2)
+print(f'o dobro de {valor} é {dobro} o triplo é {triplo} e a raiz ao quadrado é {raiz}')
