9235e5338972bd03c3dfcd5b8fd723263359a27d
Lucas Rocha <lukasrocha0402@gmail.com>
2024-03-01 09:30:52 -0300

    Adicionando o primeiro desafio do módulo operadores aritiméticos

---
+++ operadores_aritimeticos_Desa01.py
@@ -0,0 +1,5 @@
+valor = int(input('Digite o valor que deseja'))
+
+antecessor = valor - 1
+sucessor = valor +1
+print(f'O numero antecessor de {valor} é {antecessor} e o numero sucessor é {sucessor}')
