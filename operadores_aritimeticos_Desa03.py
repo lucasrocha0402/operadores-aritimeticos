df7fcf103ae68ae2664e36908bad4d591a0eb51e
Lucas Rocha <lukasrocha0402@gmail.com>
2024-03-01 09:35:26 -0300

    Adicionando o terceiro desafio

---
+++ operadores_aritimeticos_Desa03.py
@@ -0,0 +1,5 @@
+nota1 = int(input('digite o valor da primeira nota do semestre: '))
+nota2 = int(input('digite o valor da segunda nota do semestre: '))
+
+media =(nota1 + nota2) // 2
+print(f'O valor da media do aluno foi {media} a primeira nota foi {nota1} e a segunda foi {nota2}')
